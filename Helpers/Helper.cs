9380bfa2416ffc292d9e89b9ab43913c0b124e09
Bartosz O. <bartosz.osmanski.external@eviden.com>
2024-10-15 08:22:35 +0200

    Added helper method print info message / basic flow loop

---
+++ Helpers/Helper.cs
@@ -0,0 +1,12 @@
+﻿namespace Task_CLI.Helpers
+{
+    internal static class Helper
+    {
+        internal static void PrintInfoMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\n" + message);
+            Console.ResetColor();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,43 @@
-﻿namespace Task_CLI
+﻿using Task_CLI.Helpers;
+
+namespace Task_CLI
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.WriteLine("Enter command : ");
+
+                var input = Console.ReadLine() ?? string.Empty;
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    Helper.PrintInfoMessage("No input detected, try again !");
+                    continue;
+                }
+
+                var exit = false;
+
+                switch (input)
+                {
+                    case "help":
+                        Console.WriteLine("HELP !");
+                        break;
+                    case "exit":
+                        exit = true;
+                        break;
+
+                    default:
+                        break;
+                }
+
+                if (exit)
+                {
+                    break;
+                }
+            }
         }
     }
 }
